be8b3522c7d8535e925df64974b1da2dfed95ec6
haleywitherell <hwitherell22@gmail.com>
2021-10-26 20:26:19 -0400

    routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -7,37 +7,22 @@ router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
   Category.findAll({
-    // include: [Product]
+    include: [Product]
   }).then(categories => res.json(categories))
 });
 
 router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  try {
-    const categoryData = await Category.findByPk(req.params.id, {
-      // include: [Product]
-      // attributes: ?
-    });
-
-    if (!categoryData) {
-      res.status(404).json({message: 'No category found under this id'});
-    }
-
-    res.status(200).json(categoryData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  Category.findOne({
+    include: [Product]
+  }).then(categories => res.json(categories))
 });
 
 router.post('/', async (req, res) => {
   // create a new category
-  try {
-    const categoryData = await Category.create(req.body);
-    res.status(200).json(categoryData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
+  Category.create({
+  }).then(categories => res.json(categories))
 });
 
 router.put('/:id', (req, res) => {
